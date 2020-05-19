c79d603b5e9c6a70222ceb17f693379efa15ecb2
Moritz Herbert <moritz.herbert@amadeus.com>
2020-05-19 09:08:04 +0200

    chore(cocoapod): deintegrate all

--- Example/Pods/Target Support Files/Pods-nduri_Example/Pods-nduri_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_nduri_Example : NSObject
-@end
-@implementation PodsDummy_Pods_nduri_Example
-@end
--- Example/Pods/Target Support Files/nduri/nduri-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_nduri : NSObject
-@end
-@implementation PodsDummy_nduri
-@end
