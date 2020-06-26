091c07bcad1cd87ec742c4ef627911395c76e6ea
Moritz Herbert <moritz.herbert@amadeus.com>
2020-06-26 08:17:46 +0200

    feat(gesturerecognizer): add function to determine the leaf view of the ui hirarchy tree where a uitouch was recognized

--- Sources/nduri/GenericGestureRecognizer.swift
+++ Sources/nduri/GenericGestureRecognizer.swift
@@ -291,3 +291,22 @@ extension CGFloat {
         return numberFormatter.string(from: NSNumber(nonretainedObject: self)) ?? ""
     }
 }
+
+extension UITouch {
+    ///pass in window in case you dont know a more exact location to start searching for the very child view containing the touch
+    func frameOfTouchedView(startingIn view: UIView) -> CGRect? {
+        let absoluteLocation = location(in: nil)
+
+        if view.subviews.isEmpty, view.frame.contains(absoluteLocation) {
+            return view.frame
+        }
+
+        for (_, subview) in view.subviews.enumerated() {
+            if subview.frame.contains(absoluteLocation) {
+                return frameOfTouchedView(startingIn: subview)
+            }
+        }
+
+        return UIApplication.shared.keyWindow?.frame ?? nil
+    }
+}
