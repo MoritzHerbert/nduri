19cf656abae1258039107acacdfcc0504cf95430
Moritz Herbert <moritz.herbert@amadeus.com>
2020-06-30 07:51:04 +0200

    chore(swiftformat): auto-reformat code

--- Package.swift
+++ Package.swift
@@ -3,26 +3,16 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "nduri",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "nduri",
-            targets: ["nduri"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "nduri",
-            dependencies: []),
-        .testTarget(
-            name: "nduriTests",
-            dependencies: ["nduri"]),
-    ]
-)
+let package = Package(name: "nduri",
+                      products: [// Products define the executables and libraries produced by a package, and make them visible to other packages.
+                          .library(name: "nduri",
+                                   targets: ["nduri"])],
+                      dependencies: [// Dependencies declare other packages that this package depends on.
+                          // .package(url: /* package url */, from: "1.0.0"),
+                      ],
+                      targets: [// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+                          // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+                          .target(name: "nduri",
+                                  dependencies: []),
+                                .testTarget(name: "nduriTests",
+                                            dependencies: ["nduri"])])
